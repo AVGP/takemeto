792f906c82ad8629b2297ec381380f61edc99ab9
Martin N <mr.avgp@googlemail.com>
2014-09-20 18:42:33 +0000

    Filling in the quick trip controller and template

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,12 @@
 angular.module('tmt.controllers', [])
 
-.controller('QuickCtrl', function($scope, Stations, FavouriteRoutes) {
+.controller('QuickCtrl', function($scope, $http, Connections, Stations, FavouriteRoutes) {
     $scope.nearby       = [];
-    $scope.from         = null;
     $scope.destinations = [];
+    $scope.connections  = [];
+
+    $scope.from  = null;
+    $scope.route = null;
 
     navigator.geolocation.getCurrentPosition(function(position) {
         console.log("Acquired position: ", position);
@@ -20,9 +23,16 @@ angular.module('tmt.controllers', [])
     $scope.getDestinations = function() {
       console.log("Getting destinations for ", $scope.from);
       $scope.destinations = FavouriteRoutes.getByStartStation($scope.from.name);
-      $scope.to = $scope.destinations[0];
+      $scope.route = $scope.destinations[0];
     }
 
+    $scope.getConnections = function() {
+      console.log("Going to...", $scope.route);
+      $http({method: "GET", url: "http://transport.opendata.ch/v1/connections?limit=5&from=" + $scope.from.name + "&to=" + $scope.route.to})
+        .success(function(result) {
+          $scope.connections = Connections.format(result);
+        });
+    }
 })
 
 .controller('NavigationCtrl', function($scope) {
