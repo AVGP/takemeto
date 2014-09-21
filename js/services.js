5388a5f584a08865872c3cc4ee1c2fbdec058ab7
Martin N <mr.avgp@googlemail.com>
2014-09-21 15:59:58 +0000

    Completed the FavouriteRoutes service

--- js/services.js
+++ js/services.js
@@ -17,7 +17,7 @@ angular.module('tmt.services', [])
     }
   };
 })
-.factory('Connections', function($http) {
+.factory('Connections', function($http, FavouriteRoutes) {
   var formatDuration = function(duration) {
     var formatted = "";
     var days  = parseInt(duration.slice(0,2), 10),
@@ -39,17 +39,27 @@ angular.module('tmt.services', [])
   return {
     format: function(rawConnections) {
       var connections = rawConnections.connections;
+      var favConns = FavouriteRoutes.all();
       for(var i=0, len = connections.length; i<len; i++) {
         var departureAt = new Date(connections[i].from.departure),
             arrivalAt   = new Date(connections[i].to.arrival),
-            duration    = formatDuration(connections[i].duration);
+            duration    = formatDuration(connections[i].duration),
+            isFavourite = false;
+
+        for(var f=0, flen = favConns.length; f<flen;f++) {
+          if(connections[i].from.station.name == favConns[f].from && connections[i].to.station.name == favConns[f].to) {
+            isFavourite = true;
+            break;
+          }
+        }
 
         connections[i] = {
           from: connections[i].from.station.name,
           to:   connections[i].to.station.name,
           departure: departureAt.toLocaleTimeString().slice(0, -3),
           arrival: arrivalAt.toLocaleTimeString().slice(0, -3),
-          duration: duration
+          duration: duration,
+          isFavourite: isFavourite
         };
       }
 
@@ -76,20 +86,23 @@ angular.module('tmt.services', [])
   };
 })
 .factory('FavouriteRoutes', function() {
-  var favourites = [
-    {from: "Zürich HB", to: "Zürich Hardbrücke"},
-    {from: "Zürich HB", to: "Zürich Flughafen"},
-    {from: "Zürich HB", to: "Altstetten"},
-    {from: "Zürich HB", to: "Altstetten"},
-    {from: "Zürich, Räffelstrasse", to: "Zürich, Sackzelg"},
-    {from: "Zürich, Räffelstrasse", to: "Zürich HB"},
-    {from: "Zürich, Siemens", to: "Zürich HB" }
-  ];
+  var favourites = [];
 
   return {
     all: function() { return favourites; },
     getByStartStation: function(station) {
       return favourites.filter(function(fav) { return fav.from == station });
-    }
+    },
+    add: function(connection) { favourites.push(connection); return favourites; },
+    remove: function(connection) {
+      console.log("Removing ", connection);
+      for(var i=0, len = favourites.length; i<len; i++) {
+        if(connection.from == favourites[i].from && connection.to == favourites[i].to) {
+          favourites.splice(i, 1);
+          console.log("Removed", i, favourites);
+          return favourites;
+        }
+      }
+    }    
   };
 });
