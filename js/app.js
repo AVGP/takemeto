18453c873c3071c35e8d9b761b47c43cd053621e
Martin N <mr.avgp@googlemail.com>
2014-09-21 21:41:18 +0000

    Removed comments and add spacing in app states

--- js/app.js
+++ js/app.js
@@ -15,21 +15,12 @@ angular.module('tmt', ['ionic', 'tmt.controllers', 'tmt.services'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
-
-    // setup an abstract state for the tabs directive
     .state('tab', {
       url: "/tab",
       abstract: true,
       templateUrl: "templates/tabs.html"
     })
-
-    // Each tab has its own nav history stack:
 
     .state('tab.quick', {
       url: '/quick',
@@ -50,6 +41,7 @@ angular.module('tmt', ['ionic', 'tmt.controllers', 'tmt.services'])
         }
       }
     })
+
     .state('tab.schedule', {
       url: '/schedule',
       views: {
@@ -59,6 +51,7 @@ angular.module('tmt', ['ionic', 'tmt.controllers', 'tmt.services'])
         }
       }
     })
+
     .state('tab.stations', {
       url: '/stations',
       views: {
